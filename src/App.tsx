f754ade8be15d5c9b41451ffc643370180febce7
Gustavo Ribeiro Martins <gustavoribeirom2001@gmail.com>
2025-05-13 21:54:16 -0300

    componentisa pool

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Pool from './Pool'
 
 function App() {
   const [pool, setPool] = useState({ value: 0, name: "coins" })
 
 
   return (
-    <div className='pool'>
-      <div>
-        value: {pool.value}
-      </div>
-      <div>
-        name: {pool.name}
-      </div>
-    </div>
-
+    <>
+    
+    <Pool name="Pool A" value={1} />
+    <Pool name="Pool B" value={2} />
+    <Pool name="Pool C" value={3} />
+    <Pool name="Pool D" value={4} />
+   
+</>
 
   )
 }
---
+++ src/Pool.tsx
@@ -0,0 +1,10 @@
+export default function Pool (props){
+     return <div className='pool'>
+      <div>
+        value: {props.value}
+      </div>
+      <div>
+        name: {props.name}
+      </div>
+    </div>
+}
