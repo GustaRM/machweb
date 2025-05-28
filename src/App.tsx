5da0223c667cd8a9a1e1c6dbc5ee7d2c96c7c758
Gustavo Ribeiro Martins <gustavoribeirom2001@gmail.com>
2025-05-27 21:55:10 -0300

    muda para vetor

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,27 @@ import './App.css'
 import Pool from './Pool'
 
 function App() {
-  const [pool, setPool] = useState({ value: 10, name: "coins" })
+  const [pools, setPools] = useState([{ value: 10, name: "coins" }, { value: 5, name: "potions" }])
 
   return (
     <>
-    
-    <Pool name={pool.name} value={pool.value} />
-    <Pool name="Pool A" value={1} />
+      {pools.map((pool) => { return <Pool key={pool.name} name={pool.name} value={pool.value} /> })}
 
-    <Pool name="Pool B" value={2} />
-    <Pool name="Pool C" value={3} />
-    <Pool name="Pool D" value={4} />
-    <button onClick={()=>{
-      setPool({...pool, value: pool.value + 1})
-    }}>
-      Aumenta
-    </button>
-   
-</>
+      <Pool name="Pool A" value={1} />
+
+      <Pool name="Pool B" value={2} />
+      <Pool name="Pool C" value={3} />
+      <Pool name="Pool D" value={4} />
+      <button onClick={() => {
+        const novoPools = [...pools];
+        novoPools[0].value++;
+        novoPools[1].value--;
+        setPools(novoPools);
+      }}>
+        Aumenta
+      </button>
+
+    </>
 
   )
 }
